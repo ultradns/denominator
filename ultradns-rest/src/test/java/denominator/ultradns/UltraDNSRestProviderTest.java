c7c761bacc8bc7d0bfba399f5d5446ff39b3ee67
Sathish VC <sathish.vc@neustar.biz>
2017-03-14 12:38:13 +0530

    Added the test case testUltraDNSRestMetadata() in UltraDNSRestProviderTest

---
+++ ultradns-rest/src/test/java/denominator/ultradns/UltraDNSRestProviderTest.java
@@ -0,0 +1,27 @@
+package denominator.ultradns;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import java.util.Arrays;
+import denominator.Provider;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class UltraDNSRestProviderTest {
+
+  @Rule
+  public final ExpectedException thrown = ExpectedException.none();
+
+  private static final Provider PROVIDER = new UltraDNSRestProvider();
+
+  @Test
+  public void testUltraDNSRestMetadata() {
+    assertThat(PROVIDER.name()).isEqualTo("ultradnsrest");
+    assertThat(PROVIDER.supportsDuplicateZoneNames()).isFalse();
+    assertThat(PROVIDER.credentialTypeToParameterNames())
+        .containsEntry("password", Arrays.asList("username", "password"));
+  }
+
+}

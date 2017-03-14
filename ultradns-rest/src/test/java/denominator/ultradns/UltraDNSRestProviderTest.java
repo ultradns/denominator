acce2891d68d81316970434c2d6466b48330577f
Sathish VC <sathish.vc@neustar.biz>
2017-03-14 13:31:51 +0530

    Added the test case testProviderWiresUltraDNSRestZoneApi() in UltraDNSRestProviderTest

--- ultradns-rest/src/test/java/denominator/ultradns/UltraDNSRestProviderTest.java
+++ ultradns-rest/src/test/java/denominator/ultradns/UltraDNSRestProviderTest.java
@@ -1,12 +1,19 @@
 package denominator.ultradns;
 
+import denominator.DNSApiManager;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
 import java.util.Arrays;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import denominator.Credentials.MapCredentials;
 import denominator.Provider;
 
+import static denominator.CredentialsConfiguration.credentials;
+import static denominator.Denominator.create;
 import static denominator.Providers.list;
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -28,6 +35,20 @@ public class UltraDNSRestProviderTest {
   @Test
   public void testUltraDNSRestRegistered() {
     assertThat(list()).contains(PROVIDER);
+  }
+
+  @Test
+  public void testProviderWiresUltraDNSRestZoneApi() {
+    DNSApiManager manager = create(PROVIDER, credentials("username", "password"));
+    assertThat(manager.api().zones()).isInstanceOf(UltraDNSRestZoneApi.class);
+    manager = create("ultradnsrest", credentials("username", "password"));
+    assertThat(manager.api().zones()).isInstanceOf(UltraDNSRestZoneApi.class);
+
+    Map<String, String> map = new LinkedHashMap<String, String>();
+    map.put("username", "U");
+    map.put("password", "P");
+    manager = create("ultradnsrest", credentials(MapCredentials.from(map)));
+    assertThat(manager.api().zones()).isInstanceOf(UltraDNSRestZoneApi.class);
   }
 
 }

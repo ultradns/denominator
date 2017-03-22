3fc6d2df081d4f8d6db04009a6c7211fe09eaaa3
Sathish VC <sathish.vc@neustar.biz>
2017-03-22 13:23:35 +0530

    Bug fix: effectiveCodeForGeo was null because constructor in Region was not being called to create objects. Fixed it.

--- ultradns-rest/src/test/java/denominator/ultradns/model/RegionTest.java
+++ ultradns-rest/src/test/java/denominator/ultradns/model/RegionTest.java
@@ -62,7 +62,7 @@ public class RegionTest {
     public void testToString() throws Exception {
         Region region = new Region("South America", "SAM", "Region", 12);
         region.setEffectiveCode();
-        assertThat(region.toString()).isEqualTo("{\"code\": \"SAM\", \"name\": \"South America\", \"type\": \"Region\", \"id\": 12, \"effectiveCode\": \"SAM\"}");
+        assertThat(region.toString()).isEqualTo("{\"code\": \"SAM\", \"name\": \"South America\", \"type\": \"Region\", \"id\": 12, \"effectiveCode\": \"SAM\", \"effectiveCodeForGeo\": \"SAM\"}");
     }
 
     @Test
